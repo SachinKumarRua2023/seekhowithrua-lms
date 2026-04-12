60af6c3d9b355c41a9bfa6b8986756ace813cab9
Master Rua <seekhowithrua@gmail.com>
2026-04-12 18:03:36 +0530

    Fix Vercel API - convert to CommonJS, fix routes

--- seekhowithrua-aiProfessor/api/chat.js
+++ seekhowithrua-aiProfessor/api/chat.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function - Gemma 4 Chat API
-import axios from 'axios';
+const axios = require('axios');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
